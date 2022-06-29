b38541b27cd2f6358be54b3dce4231d88bd9d2a6
mouhcine <mouhcine15.00@gmail.com>
2022-06-29 16:28:07 +0200

    milestone 1 completata

--- index.php
+++ index.php
@@ -10,14 +10,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <?php
     foreach($dischi as $disco) { ?>
-        <div><img src="<?= $disco['poster'] ?>"/></div>
-        <div><?= $disco['title'] ?></div>
-        <div><?= $disco['author'] ?></div>
-        <div><?= $disco['year'] ?></div>
+        <div>
+            <div><img src="<?= $disco['poster'] ?>"/></div>
+            <div><?= $disco['title'] ?></div>
+            <div><?= $disco['author'] ?></div>
+            <div><?= $disco['year'] ?></div>
+        </div>
         <?
     } ?>
 </body>
